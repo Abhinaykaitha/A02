8830e3c029c1b5c01602069f24c9c51a94ffa4b2
Abhinay <abhinayreddy1278@gmail.com>
2018-10-07 17:54:28 -0500

    commit;

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,36 +7,36 @@ var N_SIZE = 3,
 
 
 function init() {
-  var board = document.createElement('table');
-  board.setAttribute('border', 1);
-  board.setAttribute('cellspacing', 0);
+  var b1 = document.createElement('table');
+  b1.setAttribute('border', 1);
+  b1.setAttribute('c1spacing', 0);
 
   var identifier = 1;
   for (var i = 0; i < N_SIZE; i++) {
-    var row = document.createElement('tr');
-    board.appendChild(row);
+    var r = document.createElement('tr');
+    b1.appendChild(r);
     for (var j = 0; j < N_SIZE; j++) {
-      var cell = document.createElement('td');
-      cell.setAttribute('height', 120);
-      cell.setAttribute('width', 120);
-      cell.setAttribute('align', 'center');
-      cell.setAttribute('valign', 'center');
-      cell.classList.add('col' + j, 'row' + i);
+      var c1 = document.createElement('td');
+      c1.setAttribute('height', 120);
+      c1.setAttribute('width', 120);
+      c1.setAttribute('align', 'center');
+      c1.setAttribute('valign', 'center');
+      c1.classList.add('col' + j, 'r' + i);
       if (i == j) {
-        cell.classList.add('diagonal0');
+        c1.classList.add('diagonal0');
       }
       if (j == N_SIZE - i - 1) {
-        cell.classList.add('diagonal1');
+        c1.classList.add('diagonal1');
       }
-      cell.identifier = identifier;
-      cell.addEventListener('click', set);
-      row.appendChild(cell);
-      boxes.push(cell);
+      c1.identifier = identifier;
+      c1.addEventListener('click', set);
+      r.appendChild(c1);
+      boxes.push(c1);
       identifier += identifier;
     }
   }
 
-  document.getElementById('tictactoe').appendChild(board);
+  document.getElementById('tictactoe').appendChild(b1);
   startNewGame();
 }
 
@@ -55,9 +55,9 @@ function startNewGame() {
 
 
 function win(clicked) {
-  var memberOf = clicked.className.split(/\s+/);
-  for (var i = 0; i < memberOf.length; i++) {
-    var testClass = '.' + memberOf[i];
+  var member = clicked.className.split(/\s+/);
+  for (var i = 0; i < member.length; i++) {
+    var testClass = '.' + member[i];
     var items = contains('#tictactoe ' + testClass, turn);
     if (items.length == N_SIZE) {
       return true;
